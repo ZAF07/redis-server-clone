1e22ca9b1f390fcf9d752d871c2a64bc3c19655e
ZAF07 <zaffere07@gmail.com>
2024-04-07 22:27:30 +0800

    pass 2nd stage. Simple TCP ping server

--- app/server.go
+++ app/server.go
@@ -18,9 +18,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		handler(conn)
 	}
+}
+
+func handler(conn net.Conn) {
+	// write to conn
+	conn.Write([]byte("PONG"))
 }
